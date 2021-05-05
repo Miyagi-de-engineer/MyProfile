a736e902165ccc347b478a73bf827a93a017df84
Miyagi-de-engineer <t.hayato15@gmail.com>
2021-05-05 23:36:34 +0900

    swiper.jsの導入
    基本的なスライダーの設定を行いました。
    ディレイの速度や画像イメージ等は別途追記いたします。

---
+++ dist/js/swiper.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var mySwiper = new Swiper('.swiper-container', {
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+    reverseDirection: false
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
